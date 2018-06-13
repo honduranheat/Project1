62d95a7b1e9f993de8c1d454e44ca7600c9b6548
Justin <jwhittington92@yahoo.com>
2018-06-12 21:28:04 -0400

    user info and recipe in database

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'Ezra0827'
+    },
+	'database': 'cheftest1',
+    'users_table': 'users'
+};
